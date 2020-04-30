b02c65cd199feac01682cda7ad29898ae87af709
danielm1405 <danielm1405@gmail.com>
2020-04-30 15:10:57 +0200

    basic validation for EditTexts

--- app/src/main/java/com/example/communityfavouraider/AddFavourActivity.kt
+++ app/src/main/java/com/example/communityfavouraider/AddFavourActivity.kt
@@ -4,14 +4,15 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
 class AddFavourActivity : AppCompatActivity(), View.OnClickListener {
 
-    private var favourTitle: EditText? = null
-    private var favourDescription: EditText? = null
-    private var favourCity: EditText? = null
+    private lateinit var favourTitle: EditText
+    private lateinit var favourDescription: EditText
+    private lateinit var favourCity: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +21,8 @@ class AddFavourActivity : AppCompatActivity(), View.OnClickListener {
         favourTitle = findViewById(R.id.add_favour_title)
         favourDescription = findViewById(R.id.add_favour_description)
         favourCity = findViewById(R.id.add_favour_city)
+
+        setValidators()
 
         // set on click listeners for all the buttons
         findViewById<Button>(R.id.add_favour_submit).setOnClickListener(this)
@@ -33,11 +36,39 @@ class AddFavourActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
+    private fun setValidators() {
+        favourTitle.validate("Title should have at least 10 characters.") {
+                s -> s.length >= 10
+        }
+
+        favourDescription.validate("Description should have at least 20 characters.") {
+                s -> s.length >= 20
+        }
+
+        favourCity.validate("City name should be Warsaw or Cracow.") {
+            // TODO: real validation
+                s -> s == "Warsaw" || s == "Cracow"
+        }
+    }
+
+    private fun isAnyEditTextInErrorState(): Boolean {
+        return favourTitle.error != null ||
+                favourDescription.error != null ||
+                favourCity.error != null
+    }
+
     private fun onCancelClicked() {
         onBackPressed()
     }
 
     private fun onSubmitClicked() {
-        // TODO: implement submission
+        if (isAnyEditTextInErrorState()) {
+            // TODO: make popup window
+            findViewById<TextView>(R.id.add_favour_text).text = "Error occured!"
+
+            return
+        }
+
+        findViewById<TextView>(R.id.add_favour_text).text = "OK!"
     }
 }
