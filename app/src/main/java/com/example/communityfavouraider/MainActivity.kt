234aca6e1d20c7bbea59b552623f29bdc09ec79f
danielm1405 <danielm1405@gmail.com>
2020-05-01 14:44:24 +0200

    basic login with email

--- app/src/main/java/com/example/communityfavouraider/MainActivity.kt
+++ app/src/main/java/com/example/communityfavouraider/MainActivity.kt
@@ -3,25 +3,69 @@ package com.example.communityfavouraider
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
+
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.lifecycle.ViewModelProvider
+
+import com.example.communityfavouraider.viewmodel.MainActivityViewModel
+
+import com.firebase.ui.auth.AuthUI
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+
+import java.util.*
+
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private val TAG = "MainActivity"
+    private val RC_SIGN_IN = 9001
+
+    private lateinit var viewModel: MainActivityViewModel
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        FirebaseFirestore.setLoggingEnabled(true)
+
+        viewModel = ViewModelProvider(this)[MainActivityViewModel::class.java]
+
         findViewById<View>(R.id.add_button).setOnClickListener(this)
+
+        if (shouldStartSignIn()) {
+            startSignIn()
+        }
+
+        findViewById<TextView>(R.id.main_text).text = FirebaseAuth.getInstance().currentUser?.uid.toString()
     }
 
     override fun onClick(v: View?) {
         when (v!!.id) {
-            R.id.add_button -> addClicked(v)
+            R.id.add_button -> onAddClicked(v)
         }
     }
 
-    fun addClicked(v: View?) {
+    private fun shouldStartSignIn(): Boolean {
+        return !viewModel.isSigningIn &&
+                FirebaseAuth.getInstance().currentUser == null
+    }
+
+    private fun startSignIn() {
+        val intent = AuthUI.getInstance().createSignInIntentBuilder()
+            .setAvailableProviders(Collections.singletonList(
+                AuthUI.IdpConfig.EmailBuilder().build()))
+            .setIsSmartLockEnabled(false)
+            .build();
+
+        startActivityForResult(intent, RC_SIGN_IN)
+        viewModel.isSigningIn = true
+    }
+
+    private fun onAddClicked(v: View?) {
         val intent = Intent(this, AddFavourActivity::class.java)
         startActivity(intent)
     }
