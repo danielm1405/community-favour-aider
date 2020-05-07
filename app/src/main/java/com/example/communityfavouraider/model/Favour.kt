3710f8556339b90f8a5878a13a82fcce18133afc
danielm1405 <39591259+danielm1405@users.noreply.github.com>
2020-05-07 14:32:27 +0200

    Feature/main activity view (#3)
    * add timestamp to favour model
    
    * favours are dispalyed in main activity as recycler view - still bugged
    
    * favours in main activity are clickable
    
    * main view improved

--- app/src/main/java/com/example/communityfavouraider/model/Favour.kt
+++ app/src/main/java/com/example/communityfavouraider/model/Favour.kt
@@ -1,14 +1,15 @@
 package com.example.communityfavouraider.model
 
 import com.google.firebase.firestore.IgnoreExtraProperties
+import com.google.firebase.firestore.ServerTimestamp
+import java.util.*
 
 @IgnoreExtraProperties
-data class Favour(val userId: String,
+data class Favour(val userId: String = "",
                   val userName: String = "",
                   var title: String = "",
                   var description: String = "",
                   // TODO: adapt location information to GoogleMapsAPI
-                  var city: String = ""
-)
-{
-}
+                  var city: String = "",
+                  @ServerTimestamp
+                  var timeStamp: Date? = null)
