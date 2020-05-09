b49cd28e9ef214599eba4801cfc0de10d281c538
danielm1405 <danielm1405@gmail.com>
2020-05-09 13:08:18 +0200

    map with location shown in favour details activity

--- app/src/main/java/com/example/communityfavouraider/FavourDetailsActivity.kt
+++ app/src/main/java/com/example/communityfavouraider/FavourDetailsActivity.kt
@@ -2,17 +2,22 @@ package com.example.communityfavouraider
 
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.communityfavouraider.model.Favour
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.firestore.*
 import java.text.SimpleDateFormat
 
 
 class FavourDetailsActivity : AppCompatActivity(),
-                              View.OnClickListener,
+                              OnMapReadyCallback,
                               EventListener<DocumentSnapshot> {
 
     private val TAG = "FavourDetailsActivity"
@@ -21,11 +26,14 @@ class FavourDetailsActivity : AppCompatActivity(),
         const val KEY_FAVOUR_ID = "key_favour_id"
     }
 
+    private var map: GoogleMap? = null
+
     private lateinit var favourTitle: TextView
     private lateinit var favourDescription: TextView
     private lateinit var favourUserName: TextView
     private lateinit var favourModificationDate: TextView
     private lateinit var favourAdress: TextView
+    private var favourLatLng: LatLng? = null
 
     private lateinit var favourRef: DocumentReference
     private lateinit var favourRegistration: ListenerRegistration
@@ -37,13 +45,17 @@ class FavourDetailsActivity : AppCompatActivity(),
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favour_detail)
 
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.favour_details_location_map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
         favourTitle = findViewById(R.id.favour_details_title)
         favourDescription = findViewById(R.id.favour_details_description)
         favourUserName = findViewById(R.id.favour_details_user_name)
         favourModificationDate = findViewById(R.id.favour_details_modification_date)
         favourAdress = findViewById(R.id.favour_details_location_adress)
 
-        findViewById<Button>(R.id.favour_details_accept).setOnClickListener(this)
+        findViewById<Button>(R.id.favour_details_accept).setOnClickListener { onAcceptClicked() }
 
         val favourId: String = intent.getStringExtra(KEY_FAVOUR_ID)
             ?: throw IllegalArgumentException("Must pass extra $KEY_FAVOUR_ID")
@@ -65,12 +77,6 @@ class FavourDetailsActivity : AppCompatActivity(),
         favourRegistration.remove()
     }
 
-    override fun onClick(v: View?) {
-        when (v?.id) {
-            R.id.favour_details_accept -> onAcceptClicked()
-        }
-    }
-
     override fun onEvent(snapshot: DocumentSnapshot?, e: FirebaseFirestoreException?) {
         if (e != null) {
             Log.w(TAG, "favour:onEvent", e)
@@ -84,11 +90,39 @@ class FavourDetailsActivity : AppCompatActivity(),
         favourUserName.text = favour?.userName
         favourModificationDate.text = dateFormatter.format(favour?.timeStamp)
         favourAdress.text = favour?.adress
+        favourLatLng = LatLng(favour!!.latitiude, favour.longitude)
+
+        if (map == null) {
+            Log.w(TAG, "Map is null in 'onEvent'.")
+        }
+
+        centerCameraOnLocationAndSetMarker()
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        Log.i(TAG, "Inside onMapReady")
+
+        map = googleMap
+
+        if (favourLatLng == null) {
+            Log.w(TAG, "favourLatLng is null in 'onMapReady'.")
+        }
+
+        centerCameraOnLocationAndSetMarker()
     }
 
     private fun onAcceptClicked() {
         Log.w(TAG, "Accept clicked!")
         // TODO: implement
         findViewById<Button>(R.id.favour_details_accept).text = "Clicked - TODO: impelement"
+    }
+
+    private fun centerCameraOnLocationAndSetMarker() {
+        if (favourLatLng == null) {
+            return
+        }
+
+        map?.moveCamera(CameraUpdateFactory.newLatLngZoom(favourLatLng, 15F))
+        map?.addMarker(MarkerOptions().position(favourLatLng!!))
     }
 }
